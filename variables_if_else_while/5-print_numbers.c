53c227cf13f279bb85ac48f298d4e01529042a88
Yo13038 <13038@holbertonstudents.com>
2026-02-23 19:00:51 +0100

    program that prints all single digit numbers between 0 and 10

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+* main - enter point
+*
+*print all single digit from 0 to 10
+*Return: 0 (Succes)
+*/
+int main(void)
+{
+	int digit;
+	for (digit = 0; digit < 10; n++)
+	{
+		printf("%d", digit);
+	}
+
+	printf("\n");
+	return (0)
+}
