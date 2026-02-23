22d4e3ffa5045dc0118711b4479c1fecc02ed9a3
Yo13038 <13038@holbertonstudents.com>
2026-02-23 18:05:02 +0100

    program that prints the alphabet in lowercase, and then in uppercase

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Point d'entrée
+ * Descriptif: show the alphabet in lowercase first then uppercase.
+ * Return 0 (Succes)
+ */
+int main(void)
+{
+	char ch;
+
+	/* lowercase */
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+
+	/* Uppercase */
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+	/* back to the line */
+	putchar('\n');
+
+	return (0);
+}
