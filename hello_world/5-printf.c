68885484475e9bbe2ac2082dfdeb18e7c15d0a5c
Yo13038 <13038@holbertonstudents.com>
2026-02-23 13:50:14 +0100

    C program that print a sentence with printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -5,11 +5,11 @@
  * Description: Prints with printf a specific string followed by a new line
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 
-	return 0;
+	return (0);
 }
 
