2fa05231ebe1c7f50a15e7fa51411e80dd62601b
Yo13038 <13038@holbertonstudents.com>
2026-02-23 11:51:32 +0100

    C program who print a charactere chain

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,8 @@
-#!/bin/bash
-
 #include <stdio.h>
 
 int main(void)
 {
-	put("Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle");
 
 
 	return (0);
