7c0fb28a191575dc0b61fd93c4cff58e3865f76b
Yo13038 <13038@holbertonstudents.com>
2026-02-23 18:47:42 +0100

    program that prints the alphabet in lowercase exept for q and e

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /* main - enter point
+*
 * Write a program that prints the alphabet in lowercase exept for q and e.
-*Return: 0 (Succes)
+* Return: 0 (Succes)
 */
 int main(void)
 {
