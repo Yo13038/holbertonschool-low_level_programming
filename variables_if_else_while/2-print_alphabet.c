50bb86cca342a9d0cca4038a5060e1f83edf0915
Yo13038 <13038@holbertonstudents.com>
2026-02-23 17:56:40 +0100

    program that prints the alphabet in lowercase, followed by a new line

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include<dstdio.h>
+/**
+ * main - Enter point
+ *
+ * Descriptif: show the alphabet in lowercase letter using putchar.
+ * Return: Toujours 0 (Succes)
+ */
+int main(void)
+{
+	char lettre;
+
+	/* Première utilisation de putchar dans une boucle */
+	for (lettre = 'a'; lettre <= 'z'; lettre++)
+	{
+		putchar(lettre);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
