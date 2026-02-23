9b409df1a59157223cafd93b972a0b9016f1d4b6
Yo13038 <13038@holbertonstudents.com>
2026-02-23 16:30:35 +0100

    program C who assign a random number to n to check if it is positif or negative

--- variables_if_esle_while/0-positive_or_negatives.c
+++ variables_if_esle_while/0-positive_or_negatives.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 /*
 This program will assign a random number to the variable n each time it is executed. 
-Complete the source code in order to print whether the number stored in the variable n is positive or negative.
+Complete the source code in order to print whether the number stored in the variable n is positive or negative.Or if it's zero
 */
 
 int main(void)
