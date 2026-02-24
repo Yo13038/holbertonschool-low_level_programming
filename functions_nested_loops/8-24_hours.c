d3462182772a5bcfde21b58ea49f1818b6fb61f5
Yo13038 <13038@holbertonstudents.com>
2026-02-24 17:39:39 +0100

    function that prints every minute of the day

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * jack_bauer - prints every minute of the day of Jack Bauer
  *
@@ -6,21 +7,18 @@
  */
 void jack_bauer(void)
 {
-    int hour;
-    int minute;
+	int hour, minute;
 
-    for (hour = 0; hour <= 23; hour++)
-    {
-
-        for (minute = 0; minute <= 59; minute ++)
-        {
-            _putchar((hour / 10) + '0');
+	for (hour = 0; hour <= 23; hour++)
+	{
+		for (minute = 0; minute <= 59; minute++)
+		{
+			_putchar((hour / 10) + '0');
 			_putchar((hour % 10) + '0');
 			_putchar(':');
 			_putchar((minute / 10) + '0');
 			_putchar((minute % 10) + '0');
 			_putchar('\n');
-        }
-        
-    }
+		}
+	}
 }
