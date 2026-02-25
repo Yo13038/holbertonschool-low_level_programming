0b5844ebb92b320b63cb8fb48d113cfee61afabf
Yo13038 <13038@holbertonstudents.com>
2026-02-25 15:16:40 +0100

    function that prints the numbers, from 0 to 9

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,8 +1,9 @@
 #include "main.h"
+
 /**
- *	print N - print all digit 
- * 
- *	Return Noting
+ * print_numbers - prints the numbers, from 0 to 9, followed by a new line
+ *
+ * Return: void
  */
 void print_numbers(void)
 {
@@ -10,8 +11,8 @@ void print_numbers(void)
 
 	for (n = 0; n <= 9; n++)
 	{
-
 		_putchar(n + '0');
 	}
-	_putchar ('\n');
+	_putchar('\n');
 }
+
