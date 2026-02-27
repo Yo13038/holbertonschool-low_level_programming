afe77498bb4f7ac0a4e61a44a4411eee26d03f1b
Yo13038 <13038@holbertonstudents.com>
2026-02-27 10:05:37 +0100

    dislay the menu

---
+++ simple-calculator/calculator.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - simple calculator
+ *
+ * Return: None (infinit loop)
+ */
+int main(void)
+{
+	printf("welcome\n");
+	printf("Simple Calculator\n");
+	printf("Add\n");
+	printf("Subtract\n");
+	printf("Multiply\n");
+	printf("Divide\n");
+	printf("Quit\n");
+	printf("Bye!");
+	return (0);
+}
