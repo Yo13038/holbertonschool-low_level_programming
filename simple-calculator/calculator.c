1dc095927bc494bd63194cfe966cc878a3bf547f
Yo13038 <13038@holbertonstudents.com>
2026-02-27 13:37:32 +0100

    add a Invalid choice to the calculator program

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -1,18 +1,32 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
  * main - simple calculator
  *
- * Return: None (infinit loop)
+ * Return: 0
  */
 int main(void)
 {
-	printf("welcome\n");
-	printf("Simple Calculator\n");
-	printf("Add\n");
-	printf("Subtract\n");
-	printf("Multiply\n");
-	printf("Divide\n");
-	printf("Quit\n");
-	printf("Bye!");
+int choiceMenu;
+	while (1)
+	{
+		printf("\n--- Simple Calculator ---\n");
+		printf("1) Add\n");
+		printf("2) Subtract\n");
+		printf("3) Multiply\n");
+		printf("4) Divide\n");
+		printf("0) Quit\n");
+		printf("What do you want to do ? ");
+		scanf("%d", &choiceMenu);
+		if (choiceMenu == 0)
+		{
+			printf("Bye!\n");
+			break;
+		}
+		else if (choiceMenu > 4)
+		{
+			printf("Invalid choice\n");
+		}
+	}
 	return (0);
 }
