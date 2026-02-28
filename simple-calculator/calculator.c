ef83ba3bfa3c930963c97c574eaea4d000e16275
Yo13038 <13038@holbertonstudents.com>
2026-02-28 11:14:48 +0100

    new version of simple calculator

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -1,5 +1,48 @@
 #include <stdio.h>
-
+/**
+ * add - adds two float
+ * @A: first float
+ * @B: second float
+ *
+ * Return: the sum of A plus B
+ */
+float add(float A, float B)
+{
+	return (A + B);
+}
+/**
+ * substract - substract two float
+ * @A: first float
+ * @B: second float
+ *
+ * Return: the difference of A minus B
+ */
+float substract(float A, float B)
+{
+	return (A - B);
+}
+/**
+ * multiply - multiply two float
+ * @A: first float
+ * @B: second float
+ *
+ * Return: the product of A time B
+ */
+float multiply(float A, float B)
+{
+	return (A * B);
+}
+/**
+ * divide - divide two float
+ * @A: first float
+ * @B: second float
+ *
+ * Return: A divided by B
+ */
+float divide(float A, float B)
+{
+	return (A / B);
+}
 /**
  * main - simple calculator
  *
@@ -8,16 +51,11 @@
 int main(void)
 {
 int choiceMenu;
-int A, B;
+float A = 0, B = 0, result = 0;
 	while (1)
 	{
-		printf("\n--- Simple Calculator ---\n");
-		printf("1) Add\n");
-		printf("2) Subtract\n");
-		printf("3) Multiply\n");
-		printf("4) Divide\n");
-		printf("0) Quit\n");
-		printf("What do you want to do ? ");
+		printf("\n--- Simple Calculator ---\n1) Add\n2) Substract\n");
+		printf("3) Multiply\n4) Divide\n0) Quit\nSelect your option please ? ");
 		scanf("%d", &choiceMenu);
 		if (choiceMenu == 0)
 		{
@@ -28,15 +66,23 @@ int A, B;
 		{
 			printf("Invalid choice\n");
 		}
-		else if (choiceMenu == 1)
+		else if (choiceMenu > 0 && choiceMenu < 5)
 		{
-			printf("Choice:1\n");
-			printf("A:\n");
-			scanf("%d", &A);
-			printf("B:\n");
-			scanf("%d", &B);
-			printf("Result: %d,", A + B);
+			printf("A: ");
+			scanf("%f", &A);
+			printf("B: ");
+			scanf("%f", &B);
+			if (choiceMenu == 1)
+				result = add(A, B);
+			else if (choiceMenu == 2)
+				result = substract(A, B);
+			else if (choiceMenu == 3)
+				result = multiply(A, B);
+			else if (choiceMenu == 4)
+				result = divide(A, B);
+			printf("%f", result);
 		}
 	}
 	return (0);
 }
+
