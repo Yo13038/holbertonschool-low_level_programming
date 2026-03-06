9011d55e921f520a0dc6ce0d72b7c22761342445
Yo13038 <13038@holbertonstudents.com>
2026-03-06 14:42:23 +0100

    program that prints its name using argv

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+/**
+ * main- main function
+ * @argc: void
+ * @argv: first argument(title file)
+ * Return: 0
+ */
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-  (void)argc;
-  printf("%s\n", argv[0]);
-  return(0);
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
 }
