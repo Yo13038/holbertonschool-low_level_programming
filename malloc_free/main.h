85511a19793d7be121bd44ae5595ba11bce7f0f6
Yo13038 <13038@holbertonstudents.com>
2026-03-09 11:12:34 +0100

    version .1 for the main.h file

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -30,3 +30,4 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 char *str_concat(char *s1, char *s2);
 char *create_array(unsigned int size, char c);
+*char *_strdup(char *str);
