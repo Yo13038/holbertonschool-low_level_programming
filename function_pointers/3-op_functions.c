6920d191b7664bf6cad80ec891e20813e92089d6
Yo13038 <13038@holbertonstudents.com>
2026-03-11 16:45:43 +0100

    operation file

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * op_add - addition
+ * op_sub - substract
+ * op_mul - multiply
+ * op_div - divide
+ * op_mod - modulo
+ */
+int op_add(int a, int b)
+	return (a + b);
+int op_sub(int a, int b)
+	return (a - b);
+int op_mul(int a, int b)
+	return (a * b);
+int op_div(int a, int b)
+	return (a / b);
+int op_mod(int a, int b);
+	return (a % b);
+
