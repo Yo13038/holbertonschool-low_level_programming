fadf5c0cf3ed180f9cc79ac517ebe68e7090607b
Yo13038 <13038@holbertonstudents.com>
2026-03-15 17:00:14 +0100

    the main file of the calculator

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,42 +1,21 @@
 #include "3-calc.h"
-
-/**
- * print_number - prints an integer using _putchar
- * @n: number to print
- */
-void print_number(int n)
-{
-	unsigned int n1;
-
-	if (n < 0)
-	{
-		_putchar('-');
-		n1 = -n;
-	}
-	else
-		n1 = n;
-
-	if (n1 / 10)
-		print_number(n1 / 10);
-
-	_putchar((n1 % 10) + '0');
-}
+#include <stdio.h>
 
 /**
  * main - performs simple operations
  * @argc: number of arguments
  * @argv: array of arguments
  *
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
 int main(int argc, char *argv[])
 {
-	int a, b, result;
+	int a, b;
 	int (*f)(int, int);
 
 	if (argc != 4)
 	{
-		_putchar('E'), _putchar('r'), _putchar('r'), _putchar('o'), _putchar('r'), _putchar('\n');
+		printf("Error\n");
 		exit(98);
 	}
 
@@ -44,21 +23,19 @@ int main(int argc, char *argv[])
 	b = atoi(argv[3]);
 	f = get_op_func(argv[2]);
 
-	if (f == NULL)
+	if (f == NULL || argv[2][1] != '\0')
 	{
-		_putchar('E'), _putchar('r'), _putchar('r'), _putchar('o'), _putchar('r'), _putchar('\n');
+		printf("Error\n");
 		exit(99);
 	}
 
 	if ((*argv[2] == '/' || *argv[2] == '%') && b == 0)
 	{
-		_putchar('E'), _putchar('r'), _putchar('r'), _putchar('o'), _putchar('r'), _putchar('\n');
+		printf("Error\n");
 		exit(100);
 	}
 
-	result = f(a, b);
-	print_number(result);
-	_putchar('\n');
+	printf("%d\n", f(a, b));
 
 	return (0);
 }
