6c27d20dca580803a536caa71e08824f6dd25a62
Yo13038 <13038@holbertonstudents.com>
2026-03-17 23:06:39 +0100

    final version of the header file for variadic function

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,11 +1,21 @@
 #ifndef VARIADIC_FUNCTIONS_H
-#define VARIADUC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 #include <stdarg.h>
+/**
+ * struct f_type - Structure mapping format character to function
+ * @type: The character representing the type (c, i, f, s)
+ * @f: The function pointer to the print function
+ */
+typedef struct f_type
+{
+	char *type;
+	void (*f)(va_list);
+} f_type;
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
- void print_strings(const char *separator, const unsigned int n, ...);
- void print_all(const char * const format, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif
