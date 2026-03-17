3454771aab610b11d729408dbd2bf2eb4b028f07
Yo13038 <13038@holbertonstudents.com>
2026-03-17 22:00:08 +0100

    function that returns 1 if the input integer is a prime number

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -7,13 +7,13 @@
  */
 int check_prime(int n, int i)
 {
-    if (i == n)
-        return (1);
+	if (i == n)
+		return (1);
 
-    if (n % i == 0)
-        return (0);
+	if (n % i == 0)
+		return (0);
 
-    return (check_prime(n, i + 1));
+	return (check_prime(n, i + 1));
 }
 
 /**
@@ -23,8 +23,8 @@ int check_prime(int n, int i)
  */
 int is_prime_number(int n)
 {
-    if (n <= 1)
-        return (0);
+	if (n <= 1)
+		return (0);
 
-    return (check_prime(n, 2));
+	return (check_prime(n, 2));
 }
